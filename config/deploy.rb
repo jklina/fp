8bca0974a53ea58412bee5f81969fe8a7fa23ce4
josh <josh@jjkportable.local>
2009-06-09 18:01:52 -0400

    changed deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+default_run_options[:pty] = true 
+
 set :application, "fixelpuckers"
 set :repository,  "ssh://hg@bitbucket.org/jklina/fixelpuckers/"
 set :user, "jklina"
@@ -6,7 +8,7 @@ set :user, "jklina"
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
- set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/var/www/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
