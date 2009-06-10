cb4cc2e8406fae33f37dbc1b245d77a47818e4d4
josh <josh@jjkportable.local>
2009-06-10 18:24:39 -0400

    Changed the deployment config to the new server.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,17 +3,18 @@ default_run_options[:pty] = true
 set :application, "fixelpuckers"
 set :repository,  "ssh://hg@bitbucket.org/jklina/fixelpuckers/"
 set :user, "jklina"
+set :use_sudo, false
 #set :scm_username, "jklina"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/home/jklina/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 set :scm, :mercurial
 
-role :app, "development.pixelfuckers.org"
-role :web, "development.pixelfuckers.org"
-role :db,  "development.pixelfuckers.org", :primary => true
+role :app, "jklina.webfactional.com"
+role :web, "jklina.webfactional.com"
+role :db,  "jklina.webfactional.com", :primary => true
