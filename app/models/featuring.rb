70908e0c4051c0c44e2ec1d3f73290bd12f6f478
josh <josh@jjkportable.local>
2009-07-24 17:50:21 -0400

    Fixed issue #52

--- app/models/featuring.rb
+++ app/models/featuring.rb
@@ -8,10 +8,10 @@ class Featuring < ActiveRecord::Base
   protected
 
   def set_submission_featured_at!
-    self.submission.update_attribute!(:featured_at, self.created_at)
+    self.submission.update_attributes!(:featured_at => self.created_at)
   end
 
   def unset_submission_featured_at!
-    self.submission.update_attribute!(:featured_at, nil)
+    self.submission.update_attributes!(:featured_at => nil)
   end
 end
