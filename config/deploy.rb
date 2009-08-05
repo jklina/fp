1bad95249894af087b9044989e8c38f92ce2b8f6
josh <josh@jjkportable.local>
2009-08-05 17:12:55 -0400

    Resolved issue #54

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,32 +1,31 @@
 default_run_options[:pty] = true
-default_environment["PATH"] = "/home/jklina/bin:/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin"
+#default_environment["PATH"] = "/home/jklina/bin:/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin"
 
-set :webfaction_username, "jklina"
-set :webfaction_db_type, "mysql"
-set :webfaction_db, "jklina_pf"
-set :webfaction_db_username, "jklina_pf"
-set :webfaction_port, "2613"
+set :rackspace_username, "josh"
+set :rackspace_db_type, "mysql"
+set :rackspace_db, "pixelfuckers"
+set :rackspace_db_username, "pf"
 set :database_yml_template, "database.example.yml"
  
 set :application, "fixelpuckers"
-set :deploy_to, "/home/#{webfaction_username}/webapps-releases/#{application}"
+set :deploy_to, "/var/www/#{application}"
  
 set :scm, :mercurial
 set :repository,  "ssh://hg@bitbucket.org/jklina/fixelpuckers/"
  
-set :user, "#{webfaction_username}"
+set :user, "#{rackspace_username}"
 set :use_sudo, false 
  
-set :domain, "jklina.webfactional.com"
+set :domain, "pixelfuckers.org"
  
 role :app, domain
 role :web, domain
 role :db,  domain, :primary => true
  
-desc "Symlink public to what webfaction expects the webroot to be"
-task :after_symlink, :roles => :web do
-  run "ln -nfs #{release_path}/public /home/#{webfaction_username}/webapps/#{application}/"
-end
+#desc "Symlink public to what rackspace expects the webroot to be"
+#task :after_symlink, :roles => :web do
+#  run "ln -nfs #{release_path}/public /home/#{rackspace_username}/webapps/#{application}/"
+#end
 
 task :after_update_code, :roles => :app do
   #Makes sure all the images for submissions, etc appear to be in the public folder. They're really symlinks to the Capistrano shared folder though ;)
@@ -51,9 +50,9 @@ namespace :deploy do
     db_config.delete('test')
     db_config.delete('development')
  
-    db_config['production']['adapter'] = "#{webfaction_db_type}"
-    db_config['production']['database'] = "#{webfaction_db}"
-    db_config['production']['username'] = "#{webfaction_db_username}"
+    db_config['production']['adapter'] = "#{rackspace_db_type}"
+    db_config['production']['database'] = "#{rackspace_db}"
+    db_config['production']['username'] = "#{rackspace_db_username}"
     db_config['production']['password'] = production_db_password
     db_config['production']['host'] = "localhost"
  
@@ -64,17 +63,17 @@ namespace :deploy do
  
   desc "Redefine deploy:start"
   task :start, :roles => :app do
-    invoke_command "LD_LIBRARY_PATH=/home/#{webfaction_username}/ruby1.8/lib/ruby/gems/1.8/gems/rmagick-2.9.1/lib:/home/#{webfaction_username}/lib /home/#{webfaction_username}/ruby1.8/lib/ruby/gems/1.8/bin/mongrel_rails start -c #{deploy_to}/current -d -e production -P /home/#{webfaction_username}/webapps/#{application}/log/mongrel.pid -p #{webfaction_port}", :via => run_method
+    invoke_command "LD_LIBRARY_PATH=/home/#{rackspace_username}/ruby1.8/lib/ruby/gems/1.8/gems/rmagick-2.9.1/lib:/home/#{rackspace_username}/lib /home/#{rackspace_username}/ruby1.8/lib/ruby/gems/1.8/bin/mongrel_rails start -c #{deploy_to}/current -d -e production -P /home/#{rackspace_username}/webapps/#{application}/log/mongrel.pid -p #{rackspace_port}", :via => run_method
   end
  
   desc "Redefine deploy:restart"
   task :restart, :roles => :app do
-    invoke_command "LD_LIBRARY_PATH=/home/#{webfaction_username}/ruby1.8/lib/ruby/gems/1.8/gems/rmagick-2.9.1/lib:/home/#{webfaction_username}/lib /home/#{webfaction_username}/ruby1.8/lib/ruby/gems/1.8/bin/mongrel_rails restart -c #{deploy_to}/current -P /home/#{webfaction_username}/webapps/#{application}/log/mongrel.pid", :via => run_method
+    invoke_command "LD_LIBRARY_PATH=/home/#{rackspace_username}/ruby1.8/lib/ruby/gems/1.8/gems/rmagick-2.9.1/lib:/home/#{rackspace_username}/lib /home/#{rackspace_username}/ruby1.8/lib/ruby/gems/1.8/bin/mongrel_rails restart -c #{deploy_to}/current -P /home/#{rackspace_username}/webapps/#{application}/log/mongrel.pid", :via => run_method
   end
  
   desc "Redefine deploy:stop"
   task :stop, :roles => :app do
-    invoke_command "LD_LIBRARY_PATH=/home/#{webfaction_username}/ruby1.8/lib/ruby/gems/1.8/gems/rmagick-2.9.1/lib:/home/#{webfaction_username}/lib /home/#{webfaction_username}/ruby1.8/lib/ruby/gems/1.8/bin/mongrel_rails stop -c #{deploy_to}/current -P /home/#{webfaction_username}/webapps/#{application}/log/mongrel.pid", :via => run_method
+    invoke_command "LD_LIBRARY_PATH=/home/#{rackspace_username}/ruby1.8/lib/ruby/gems/1.8/gems/rmagick-2.9.1/lib:/home/#{rackspace_username}/lib /home/#{rackspace_username}/ruby1.8/lib/ruby/gems/1.8/bin/mongrel_rails stop -c #{deploy_to}/current -P /home/#{rackspace_username}/webapps/#{application}/log/mongrel.pid", :via => run_method
   end
 end
 
