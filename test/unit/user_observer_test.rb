5ca7e0018d199464f3bdfce220f21990724e0bae
Henry Phan <henry@henryphan.com>
2011-02-03 15:39:31 -0800

    Added unit tests for UserObserver.

--- test/unit/user_observer_test.rb
+++ test/unit/user_observer_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class UserObserverTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "The UserObserver class" do
+    setup do
+      ActionMailer::Base.deliveries.clear
+      @user = Factory.build(:user)
+    end
+
+    should "deliver an email when calling after_create with a user" do
+      assert ActionMailer::Base.deliveries.empty?
+      UserObserver.instance.after_create(@user)
+      assert ActionMailer::Base.deliveries.any?
+    end
   end
 end
