e5ae478f3a133eceb83186b09b5c0d2d963430ed
Henry Phan <henry@henryphan.com>
2011-02-03 13:32:52 -0800

    Added unit tests for Forums.

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -13,10 +13,12 @@
 #
 
 class Forum < ActiveRecord::Base
+  attr_accessible :title, :description, :weight, :forum_group_id
+
   belongs_to :forum_group
-  has_many :topics, :dependent => :destroy, :order => 'last_post_created_at DESC'
-  has_many :posts, :dependent => :destroy
+  has_many   :topics, :dependent => :destroy, :order => 'last_post_created_at DESC'
+  has_many   :posts,  :dependent => :destroy
   
   validates_presence_of :title, :weight
-  validates_inclusion_of :weight, :in => -100..100, :message => "must be inbetween -100 and 100."
+  validates_inclusion_of :weight, :in => -100..100, :message => "must be between -100 and 100, inclusive."
 end
--- test/factories.rb
+++ test/factories.rb
@@ -14,6 +14,13 @@ Factory.define(:forum_group) do |f|
   f.weight { rand(101) }
 end
 
+Factory.define(:forum) do |f|
+  f.title       { Faker::Lorem.sentence.chomp(".") }
+  f.description { Faker::Lorem.sentences.join(" ") }
+  f.weight      { rand(101) }
+  f.association :forum_group
+end
+
 Factory.define :user do |f|
   f.username "foo"
   f.password "foobar"
