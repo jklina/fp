d846405b7d057ade51ea18c1f5fa4a0b4935839a
jklina <joshua.klina@gmail.com>
2012-01-29 11:12:24 -0500

    updating seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,21 @@
 #Create initial admin user. Probably want to replace this with a script that let's the user create their own admin. 
 
-User.find_or_create_by_username(:username => 'admin', :name => 'admins', :email => 'jlfds@jklfds.com', :access_level => 3, :password => 'secret', :password_confirmation => 'secret', :confirmed => true)
+# Create a new user
+@user = User.new
+
+# Attributes for the user
+@attrib = {
+  :username       => "admin",
+  :name       => "admin",
+  :email       => "admin@example.com",
+  :access_level       => 3,
+  :password       => 'secret',
+  :password_confirmation       => 'secret',
+  :confirmed       => true,
+}
+
+# Use 'send' to call the attributes= method on the object
+@user.send :attributes=, @attrib, false
+
+# Save the object
+@user.save
